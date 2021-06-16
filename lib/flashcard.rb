36d5ba3e83a0ad7582d378951fb6e0000ee88056
mwpenn94 <mwpenn94@gmail.com>
2021-06-15 17:02:02 -0700

    test commit

---
+++ lib/flashcard.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "flashcard/version"
+
+module Flashcard
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/flashcard_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Flashcard do
+  it "has a version number" do
+    expect(Flashcard::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
